8a595bdf38f8179d80a7c6c9e8ef30ba4af6caf5
benbpede <benbpede@gmail.com>
2022-03-31 21:00:23 +0900

    build submit

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,14 +2,24 @@ import { useState, useCallback } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback((e) => {
     setValue(e.target.value);
-  });
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value값 초기화
+      //submit 이벤트는 새로고침을 발생시키므로 이를 방지하기위해 밑 함수 사용
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요."
         value={value}
